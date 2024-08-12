a1e69e03fec676c2a546e7b2160a58a167b7f95c
tapas-code <tapas.code247@gmail.com>
2024-08-12 11:50:58 +0530

    Home Page Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,34 @@
 import React from "react";
-import HeroImg from '../assets/download.jpeg'
+import HeroImg from "../assets/download.jpeg";
+import { useAuth } from "../store/auth";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { isLoggedIn, userRole } = useAuth();
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    if (isLoggedIn) {
+      navigate(`/${userRole}`);
+    } else navigate("/login");
+  };
+  
   return (
     <div className="hero bg-base-200 flex-1">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={HeroImg}
-          className="max-w-sm rounded-lg shadow-2xl "
-        />
+        <img src={HeroImg} className="max-w-sm rounded-lg shadow-2xl " />
         <div className="lg:w-1/2">
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <h1 className="text-5xl font-bold">
+            Classhub - Classroom Management!
+          </h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            A classroom management system, offering role-specific dashboards for
+            Principals, Teachers, and Students. Key features include user
+            management, classroom assignments, and timetable viewing.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary" onClick={handleRedirect}>
+            Get Started
+          </button>
         </div>
       </div>
     </div>
